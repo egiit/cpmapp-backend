c32bd13ad1aae2e0e930b2f36048d8a9736daa49
Egi <giefilth@gmail.com>
2022-03-19 08:24:00 +0700

    db update

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('db_cpm', 'root', '', {
+const db = new Sequelize('db_cpm', 'root', 'shi@123', {
   host: 'localhost',
   dialect: 'mysql',
   logging: false,
